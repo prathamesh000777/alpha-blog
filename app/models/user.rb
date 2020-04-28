6c3546e6817b366dc14695c9a073baf9fecaae6b
Prathamesh <prathamesh000777@gmail.com>
2020-04-28 15:07:29 +0530

    Add edit template and update action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
-  before_save {self.email = email.downcase}
   validates :username, presence: true,
-                    uniqueness: {case_sensitive: false},
-                    length: {minimum: 3, maximum: 25}
-  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
-                    length: {maximum: 105},
-                    uniqueness: {case_sensitive: false},
-                    format: {with: VALID_EMAIL_REGEX }
+                      uniqueness: { case_sensitive: false },
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }, on: :create
+  
+
+
   has_secure_password
 end
